77130d56017bcc84a8e851eecce710b9dcf4b028
Sabahat Z <sabahat_zulfiqar@hotmail.com>
2019-03-29 14:56:24 +0000

    Updated Code

--- app.py
+++ app.py
@@ -7,28 +7,28 @@ cluster = Cluster(['cassandra'])
 session = cluster.connect()
 app = Flask(__name__)
 
-apikey=config.apikey
+apikey=config.apikey #api key 
 
-@app.route('/')
+@app.route('/') #Homepage
 def hello():
-	url = 'http://api.apixu.com/v1/current.json?key={}&q=London'.format(apikey)
+	url = 'http://api.apixu.com/v1/current.json?key={}&q=London'.format(apikey) 
 	resp = requests.get(url)
 	content = resp.json()
-	temp = (content['current']['temp_c'])
+	temp = (content['current']['temp_c']) #Fetch temperature using the api
 	print(temp)
-	return('<h1>Car Detail App</h1><br><br> The temperature today is {}</h1>'.format(temp))
+	return('<h1>Car Detail App</h1><br><br> The temperature today is {}</h1>'.format(temp)) #Display current temperature
 
 
-@app.route('/car/<car>')
+@app.route('/car/<car>') #Specify the car name in this path
 def profile(car):
-	rows = session.execute( """Select * From car.stats where car = '{}' ALLOW FILTERING""".format(car))
+	rows = session.execute( """Select * From car.stats where car = '{}' ALLOW FILTERING""".format(car)) #Selects the car mentioned in the path
 	for row in rows:
 		name = row.car
 		c= row.cylinders
 		w=row.weight
-		return('<h1>{} has {} cylinders and weighs {} kg!</h1>'.format(name,c,w))
+		return('<h1>{} has {} cylinders and weighs {} kg!</h1>'.format(name,c,w)) #Displays details of the mentioned car
 
-	return('<h1>That car does not exist</h1>')
+	return('<h1>That car does not exist</h1>') #Displays this message if the car does not exist
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8080)
